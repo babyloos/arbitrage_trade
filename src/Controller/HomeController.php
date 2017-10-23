6373921de7b7b29b3446790ea4558752ab5bae7f
babyloos <babyloos1990@yahoo.co.jp>
2017-10-23 10:09:39 +0000

    add coincheck lib

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,15 @@
 namespace App\Controller;
 
 class HomeController extends AppController {
+    
+    public function initialize() {
+        parent::initialize();
+         $this->loadComponent('ArbitrageCalc');
+    }
+    
+    
     public function index() {
         // echo "index";
+        echo $this->ArbitrageCalc->calc(10, 20);
     }
 }
