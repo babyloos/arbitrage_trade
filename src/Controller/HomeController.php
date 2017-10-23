e06d38635cb8261f5c8f6fc2b03589d75be8d307
babyloos <babyloos1990@yahoo.co.jp>
2017-10-23 05:42:38 +0000

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controller;
+
+class HomeController extends AppController {
+    public function index() {
+        // echo "index";
+    }
+}
---
+++ src/Template/Home/index.ctp
@@ -0,0 +1,7 @@
+<div class="menu">
+    <h1 class="title">ホーム画面</h1>
+</div>
+
+<div class="datas">
+        <p>データ表示領域</p>
+</div>
