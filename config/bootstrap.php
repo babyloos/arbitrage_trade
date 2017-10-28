6f0bb0c805ec94bd1b04c16c538579238defdaf3
babyloos <babyloos1990@yahoo.co.jp>
2017-10-28 04:57:33 +0000

    heroku用の設定を追加

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -74,6 +74,11 @@ use Cake\Utility\Security;
  * idea to create multiple configuration files, and separate the configuration
  * that changes from configuration that does not. This makes deployment simpler.
  */
+ 
+if (isset($_ENV['CAKE_ENV'])) {
+    Configure::load('app_' . $_ENV['CAKE_ENV'], 'default');
+}
+
 try {
     Configure::config('default', new PhpConfig());
     Configure::load('app', 'default', false);
@@ -219,6 +224,10 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
+// if (Configure::read('debug')) {
+//     Plugin::load('DebugKit', ['bootstrap' => true]);
+// }
+
+if (Configure::read('debug') && !isset($_ENV['CAKE_ENV'])) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
